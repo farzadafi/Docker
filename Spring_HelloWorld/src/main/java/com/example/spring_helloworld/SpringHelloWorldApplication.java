eb7b65ec0190f006cfaa5d9ac92b5d73391f80a8
farzadafi <farzadafi40@gmail.com>
2022-12-07 09:15:52 +0330

    fix hello method

--- Spring_HelloWorld/src/main/java/com/example/spring_helloworld/SpringHelloWorldApplication.java
+++ Spring_HelloWorld/src/main/java/com/example/spring_helloworld/SpringHelloWorldApplication.java
@@ -2,12 +2,20 @@ package com.example.spring_helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class SpringHelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringHelloWorldApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Docker";
+    }
 }
